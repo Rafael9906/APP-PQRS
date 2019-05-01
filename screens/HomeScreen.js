d6db32448c5936890040579f87bccbbcf3ae11dc
Rafael <rafaortiz0206@gmail.com>
2019-04-30 21:47:42 -0500

    1.0

---
+++ screens/HomeScreen.js
@@ -0,0 +1,258 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+import { StackNavigator } from 'react-navigation';
+import { TextInput } from 'react-native-gesture-handler';
+
+
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Registro de PQRS'
+  };
+
+    constructor(props){
+      super(props)
+      this.state={
+        userID:'',
+        userName:'',
+        userEmail:'',
+        userPhone: '',
+        userPQRS: '',
+        userComent: ''
+      }
+    }
+
+    InsertUsers = () => {
+      const {userID,userName,userEmail,userPhone,userPQRS,userComent} = this.state;
+
+      fetch('https://react-connection.000webhostapp.com/usuario/insert.php',{
+        method: 'POST',
+        headers:{
+          'Accept': 'application/json',
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          id: userID,
+          nombre: userName,
+          correo: userEmail,
+          telefono: userPhone,
+          pqrs: userPQRS,
+          comentario: userComent
+        })
+      }).then((response)=>response.json())
+        .then((responseJson) => {
+          alert(responseJson);
+        }).catch((error) =>{
+          console.error(error);
+        })
+
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+        <Text>DATOS PERSONALES</Text>
+
+        <TextInput keyboardType='number-pad' maxLength = {10}
+         placeholder = 'Identificación'
+         onChangeText = {TextInputValue => this.setState({userID: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle2}
+       />
+
+       <TextInput
+         placeholder = 'Nombre'
+         onChangeText = {TextInputValue => this.setState({userName: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle2}
+       />
+
+       <TextInput
+         placeholder = 'Correo electrónico'
+         onChangeText = {TextInputValue => this.setState({userEmail: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle2}
+       />
+
+       <TextInput keyboardType='number-pad' maxLength = {10}
+         placeholder = 'Teléfono'
+         onChangeText = {TextInputValue => this.setState({userPhone: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle2}
+       />
+
+        <Text>PQRS</Text>
+        <TextInput
+         placeholder = 'Tipo de PQRS'
+         onChangeText = {TextInputValue => this.setState({userPQRS: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle3}
+       />
+
+       
+
+        <TextInput
+         placeholder = 'Comentarios'
+         onChangeText = {TextInputValue => this.setState({userComent: TextInputValue})}
+         underlineColorAndroid = 'transparent'
+         style = {styles.TextInputStyle3}
+       />
+
+         <TouchableOpacity  onPress={this.InsertUsers}activeOpacity = {.4} style = {styles.TouchableOpacityStyle} >
+         <Text style={styles.TextStyle }>Registrar</Text>
+         </TouchableOpacity>
+
+         </View>
+      )}; 
+
+    }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    marginTop: 5,
+    backgroundColor: '#fff',
+  },
+  TextInputStyle:
+  {
+    textAlign: 'center',
+    marginBottom: 7,
+    width:'90%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#000'
+  },
+  TextInputStyle2:
+  {
+    textAlign: 'center',
+    marginBottom: 7,
+    marginTop: 20,
+    width:'90%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#000'
+  },
+  TextInputStyle3:
+  {
+    textAlign: 'center',
+    marginBottom: 7,
+    marginTop: 20,
+    width:'90%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#FF0000'
+  },
+
+  TextStyle: {
+    color:'#fff',
+    textAlign: 'center'
+  },
+
+  TouchableOpacityStyle: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 5,
+    marginBottom: 7,
+    width: '90%',
+    backgroundColor: '#00BCD4'
+
+  },
+
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
---
+++ screens/LinksScreen.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ListView,
+  ActivityIndicator
+} from 'react-native';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Consultar',
+  };
+
+  ShowUsers = () => {
+    this.props.navigation.navigate('')
+
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoding: true
+    }
+  }
+
+  componentDidMount(){
+    return fetch('https://react-connection.000webhostapp.com/view.php')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      let ds = new ListView.DataSource({rowHasChanged: (r1, r2)=>r1 !== r2})
+      this.setState({
+        isLoding: false,
+        dataSource: ds.cloneWithRows(responseJson)
+      },function(){})
+    }).catch((error)=>{
+      console.error(error);
+    })
+  }
+
+  Action_Click(){
+
+  }
+
+  ListViewItemSeparator = () => {
+    return(
+      <View
+      style = {{
+       height: .5,
+       width: '100%',
+       backgroundColor: '#2196F3'
+      }} 
+      /> 
+    )
+  }
+
+  render() {
+
+    if(this.state.isLoding)
+    {
+      return(
+        <View style={{flex:1, paddingTop:20}}>
+        <ActivityIndicator/>
+        
+        </View>
+      ) 
+    }
+
+    return (
+      <View style={styles.ContainerDataUser}>
+        <TouchableOpacity  onPress={this.ShowUsers}activeOpacity = {.4} style = {styles.TouchableOpacityStyle} >
+         <Text style={styles.TextStyle }>Registrar</Text>
+        </TouchableOpacity>
+
+
+        <ListView
+
+          dataSource = {this.state.dataSource}
+          renderSeparator = {this.ListViewItemSeparator}
+          renderRow = {(rowData) =>
+          <Text style = {styles.rowViewContainer} onPress = {this.Action_Click.bind(this,
+          )}>
+          {rowData.identificacion}
+          </Text>
+          }
+
+        />
+
+      </View>
+
+     
+
+    );
+  }
+}
+
+//Estilos
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+
+  TouchableOpacityStyle: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 5,
+    marginBottom: 7,
+    width: '90%',
+    backgroundColor: '#00BCD4'
+  },
+
+  ContainerDataUser: {
+    flex:1,
+    paddingTop:20,
+    marginLeft:5,
+    marginRight:5
+  },
+
+  rowViewContainer: {
+    textAlign: 'center',
+    fontSize:20,
+    paddingTop:10,
+    paddingRight:10,
+    paddingBottom:10
+  },
+
+  TextStyle: {
+    color:'#fff',
+    textAlign: 'center'
+  }
+});
