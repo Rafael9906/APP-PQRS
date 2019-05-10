e2fe966c82df2cc06078e7160a505675e20ba93f
Rafael <rafaortiz0206@gmail.com>
2019-05-10 15:54:22 -0500

    otro arreglo

--- PHP/login.php
+++ PHP/login.php
@@ -10,15 +10,12 @@
 
 	if($obj['id']!=""){
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	$result= mysqli_query($link,"SELECT * FROM users where identificacion='$id' and password='$md5Pass'");
-=======
+
 	$result= mysqli_query($link,"SELECT * FROM usuario where id='$id' and contraseña='$md5Pass'");
->>>>>>> a288021fd799efc94bd0da83dc9da789fc83f35a
-=======
+
 	$result= mysqli_query($link,"SELECT * FROM usuario where id='$id' and contraseña='$md5Pass'");
->>>>>>> parent of 6d1e01d... 1.9
 
 		if($result->num_rows==0){
 			echo json_encode('Wrong Details');
